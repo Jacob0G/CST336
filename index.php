2cf806a4ab0b5b6d99b311a0a568b48bdd59f2ef
Jacob Gull <jgull@csumb.edu>
2018-09-06 19:29:29 +0000

    php index fix

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
 <html>
     <?php
+    echo "<a href = 'Labs/Lab1/portfolio/index.html'>Lab1</a>";
     ?>
-    <head>
-        <title> Jacob Gulls CST336</title>
-    </head>
-        <nav>
-            <hr width="50%" />
-            <a href = "/CST336/Labs/Lab1/portfolio/index.html">lab 1</a>
-        </nav>
-    <body>
-    </body>
 </html>
