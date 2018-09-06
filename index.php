75befaf611d269c7efbd79689fdf02ffce6fced3
Jacob Gull <jgull@csumb.edu>
2018-09-06 19:32:42 +0000

    php index fix

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
-<html>
-    <?php
-    echo "<a href = 'Labs/Lab1/portfolio/index.html'>Lab1</a>";
-    ?>
+<html><body>
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+echo "<h1>Jacob Gull's CST336<h1>";
+echo "<a href = 'Labs/Lab1/portfolio/index.html'>Lab1</a>";
+?>
+</body>
 </html>
