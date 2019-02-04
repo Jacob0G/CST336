b12a9f4aff3db39e15717fe13327ebfcd2b84bea
Jacob Gull <jgull@csumb.edu>
2019-02-04 21:34:17 +0000

    p1

--- index.php
+++ index.php
@@ -4,7 +4,13 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 echo "<h1>Jacob Gull's CST336<h1>";
+
 echo "<a href = 'Labs/Lab1/portfolio/index.html'>Lab1</a>";
+echo "<br /><br />";
+echo "<a href = 'HW/HW1/index.html'>HW1</a>";
+echo "<br /><br />";
+echo "<a href = 'Practice/p1/index.html'>p1</a>";
+
 ?>
 </body>
 </html>
