cd14f5ab807a294ce6ae835e2a1ec8df050f6f86
Jacob Gull <jgull@csumb.edu>
2019-04-13 06:58:16 +0000

    fixing Lab 7 tweak

--- Labs/lab7/loginProcess.php
+++ Labs/lab7/loginProcess.php
@@ -21,8 +21,8 @@ $stmt = $conn->prepare($sql);
 $stmt->execute($namedParameters);
 $record = $stmt->fetch(PDO::FETCH_ASSOC); //we are expecting ONLY one record, so we use fetch instead of fetchAll
 
-$var = "Hello ";
-print_r($var);
+
+echo json_encode($record);
  
  if (empty($record)) {
      
@@ -35,8 +35,8 @@ print_r($var);
     //echo $record[0]['firstName']; //using fetchAll
     //echo $record['firstName'] . " " . $record['lastName'] ; //using fetch
     
-    $_SESSION['adminName'] = $record['firstName'] . " " . $record['lastName'];
-    header('location: admin.php'); //redirecting to a new file
+    // $_SESSION['adminName'] = $record['firstName'] . " " . $record['lastName'];
+    // header('location: admin.php'); //redirecting to a new file
     
     
 
