cec7ccfb19ad45e41db0dd5504f95f2eb6631db0
Jacob Gull <jgull@csumb.edu>
2019-04-13 07:00:10 +0000

    fixing Lab 7 tweak

--- Labs/lab7/loginProcess.php
+++ Labs/lab7/loginProcess.php
@@ -21,25 +21,24 @@ $stmt = $conn->prepare($sql);
 $stmt->execute($namedParameters);
 $record = $stmt->fetch(PDO::FETCH_ASSOC); //we are expecting ONLY one record, so we use fetch instead of fetchAll
 
-
 echo json_encode($record);
  
- if (empty($record)) {
+//  if (empty($record)) {
      
-     // echo "Username or Password are incorrect!";
-     // header("Location: login.php?LoginError=True");
+//      // echo "Username or Password are incorrect!";
+//      // header("Location: login.php?LoginError=True");
 
      
- }  else {
+//  }  else {
  
-    //echo $record[0]['firstName']; //using fetchAll
-    //echo $record['firstName'] . " " . $record['lastName'] ; //using fetch
+//     //echo $record[0]['firstName']; //using fetchAll
+//     //echo $record['firstName'] . " " . $record['lastName'] ; //using fetch
     
-    // $_SESSION['adminName'] = $record['firstName'] . " " . $record['lastName'];
-    // header('location: admin.php'); //redirecting to a new file
+//     // $_SESSION['adminName'] = $record['firstName'] . " " . $record['lastName'];
+//     // header('location: admin.php'); //redirecting to a new file
     
     
 
-}
+// }
 
 ?>
