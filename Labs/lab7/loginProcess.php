3d9f0a44b20e3c28ca0a52d5501a75655f7e1af3
Jacob Gull <jgull@csumb.edu>
2019-04-14 20:10:27 +0000

    removed phpMyAdmin

--- Labs/lab7/loginProcess.php
+++ Labs/lab7/loginProcess.php
@@ -1,9 +1,6 @@
 <?php
 session_start(); //starts or resumes an existing session
 
-error_reporting(E_ALL);
-ini_set('display_errors', 'On');
-
 // print_r($_POST); //for debugging purposes, display the content of the $_POST array
 
 include '../../inc/dbConnection.php';
@@ -17,34 +14,30 @@ $password = sha1($_POST['password']);
 $sql = "SELECT * FROM om_admin WHERE username = :username AND password = :password";
 
 $namedParameters = array();
-$namedParameters[':username'] = "$username";
-$namedParameters[':password'] = "$password";
+$namedParameters[':username'] = $username;
+$namedParameters[':password'] = $password;
 
 $stmt = $conn->prepare($sql);
-echo "Hello\n";
-$res = $stmt->execute($namedParameters);
-if (!$res)
-{
-   debug($stmt->error);
-}
-$records = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($records);
-//  if (empty($record)) {
-     
-//      // echo "Username or Password are incorrect!";
-//      // header("Location: login.php?LoginError=True");
+$stmt->execute($namedParameters);
+$record = $stmt->fetch(PDO::FETCH_ASSOC); //we are expecting ONLY one record, so we use fetch instead of fetchAll
 
-     
-//  }  else {
  
-//     //echo $record[0]['firstName']; //using fetchAll
-//     //echo $record['firstName'] . " " . $record['lastName'] ; //using fetch
+ if (empty($record)) {
+     
+     // echo "Username or Password are incorrect!";
+     header("Location: login.php?LoginError=True");
+
+     
+ }  else {
+ 
+    //echo $record[0]['firstName']; //using fetchAll
+    //echo $record['firstName'] . " " . $record['lastName'] ; //using fetch
     
-//     // $_SESSION['adminName'] = $record['firstName'] . " " . $record['lastName'];
-//     // header('location: admin.php'); //redirecting to a new file
+    $_SESSION['adminName'] = $record['firstName'] . " " . $record['lastName'];
+    header('location: admin.php'); //redirecting to a new file
     
     
 
-// }
+}
 
 ?>
