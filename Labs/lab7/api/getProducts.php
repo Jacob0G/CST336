09d8509375c553e1195fd5938d35cbb1c4c1d515
Jacob Gull <jgull@csumb.edu>
2019-04-14 20:43:15 +0000

    removed phpMyAdmin

--- Labs/lab7/api/getProducts.php
+++ Labs/lab7/api/getProducts.php
@@ -1,18 +1,13 @@
 <?php
 
-$host = "localhost";
-$dbname = "ottermart";
-$username = "root";
-$password = "";
-
-// Establishing a connection
-$dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+include '../../../inc/dbConnection.php';
+$conn = getDatabaseConnection("ottermart");
 
 // Setting Errorhandling to Exception
-$dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+// $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 
 $sql = "SELECT * FROM om_product ORDER BY productPrice LIMIT 10";
-$stmt = $dbConn -> prepare($sql);  //$connection MUST be previously initialized
+$stmt = $conn -> prepare($sql);  //$connection MUST be previously initialized
 $stmt->execute();
 $records = $stmt->fetchAll(PDO::FETCH_ASSOC); //use fetch for one record, fetchAll for multiple
 
